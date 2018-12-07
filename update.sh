024a3ebcda58c1114e098c20e3d66a78eeb2e1cb
Almog Hamdani <almogh52@gmail.com>
2018-12-08 01:59:47 +0200

    Fix an issue in the update script where it downloads sourcemod after the addons and removes some of them

--- update.sh
+++ update.sh
@@ -4,11 +4,10 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 # Update the server app using steamcmd
 ../steamcmd/steamcmd.sh +login anonymous +force_install_dir $SCRIPTPATH +app_update 740 validate +quit
 
-# Download addons
-curl -sqL "https://gitlab.com/almogh52/csgo-server-addons/-/archive/master/csgo-server-addons-master.tar.gz" | tar --transform 's,csgo-server-addons-master,.,' --show-transformed -zxvf -
-
-# Move to the csgo folder to download sourcemod
-cd csgo
-
 # Download sourcemod
+cd csgo
 curl -sqL "https://sm.alliedmods.net/smdrop/1.9/sourcemod-1.9.0-git6266-linux.tar.gz" | tar zxvf -
+
+# Download addons
+cd ..
+curl -sqL "https://gitlab.com/almogh52/csgo-server-addons/-/archive/master/csgo-server-addons-master.tar.gz" | tar --transform 's,csgo-server-addons-master,.,' --show-transformed -zxvf -
