d6804d2433115b2044470243f062cfbfc31245a6
Almog Hamdani <almogh52@gmail.com>
2018-12-08 00:06:11 +0200

    Add a script shell to update the server

---
+++ update.sh
@@ -0,0 +1,14 @@
+# Get the script's path
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# Update the server app using steamcmd
+./steamcmd +login anonymous +force_install_dir $SCRIPTPATH +app_update 740 validate +quit
+
+# Move to the csgo folder to download sourcemod and addons
+cd csgo
+
+# Download sourcemod
+curl -sqL "https://sm.alliedmods.net/smdrop/1.9/sourcemod-1.9.0-git6266-linux.tar.gz" | tar zxvf -
+
+# Download addons
+curl -sqL "https://gitlab.com/almogh52/csgo-server-addons/-/archive/master/csgo-server-addons-master.tar.gz" | tar --transform 's,csgo-server-addons-master,.,' --show-transformed -zxvf -
