68018409c8a2089b11177c54b88d22fb1d53b8d6
Almog Hamdani <almogh52@gmail.com>
2018-12-08 02:15:35 +0200

    Add install for metamod in the update script

--- update.sh
+++ update.sh
@@ -4,8 +4,9 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 # Update the server app using steamcmd
 ../steamcmd/steamcmd.sh +login anonymous +force_install_dir $SCRIPTPATH +app_update 740 +quit
 
-# Download sourcemod
+# Download metamod and sourcemod
 cd csgo
+curl -sqL "https://mms.alliedmods.net/mmsdrop/1.10/mmsource-1.10.7-git966-linux.tar.gz" | tar zxvf -
 curl -sqL "https://sm.alliedmods.net/smdrop/1.9/sourcemod-1.9.0-git6266-linux.tar.gz" | tar zxvf -
 
 # Download addons
